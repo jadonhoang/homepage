5b1a2a4d7457885ff9669eb86689a40c6ea15df5
jadonhoang <jadonhoang@gmail.com>
2021-08-03 18:48:47 -0700

    bash script

--- build_homepage.sh
+++ build_homepage.sh
@@ -1,1 +1,3 @@
-python3 homepage.py
+cat templates/top.html contents/blog.html templates/bottom.html > docs/blog.html
+cat templates/top.html contents/index.html templates/bottom.html > docs/index.html
+cat templates/top.html contents/online_resume.html templates/bottom.html > docs/online_resume.html
