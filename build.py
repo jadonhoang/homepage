de556201dd35f2914897905973c0829146b85ffa
jadonhoang <jadonhoang@gmail.com>
2021-08-12 15:58:59 -0700

    updated main function

--- build.py
+++ build.py
@@ -5,44 +5,58 @@ def read_base_html():
 	return base_html
 	
 
-
-
-def main(page):
+def title():
 	base = read_base_html()
-	content_html = open("./contents/"+page+".html").read()
-	base.replace("{{title}}", page.capitalize())
-	page_html = base.replace("{{content}}", content_html)
-	open("docs/"+page+".html", "w+").write(page_html)
-	
-	
-	
-
-main("blog")
-main("index")
-main("online_resume")
+	title = base.replace("{{title}}")
+	return title
 
 
 
 pages = [
 	{
-	"filename" : "contents/blog.html",
-	"output" : "docs/blog.html",
+	"filename" : "./contents/blog.html",
+	"output" : "./docs/blog.html",
 	"title" : "Blog",
 	},
 	{
-	"filename" : "contents/index.html",
-	"output" : "docs/index.html",
+	"filename" : "./contents/index.html",
+	"output" : "./docs/index.html",
 	"title" : "About Me",
 	},
 	{
-	"filename" : "contents/online_resume.html",
-	"output" : "docs/online_resume.html",
+	"filename" : "./contents/online_resume.html",
+	"output" : "./docs/online_resume.html",
 	"title" : "Resume",
 	}
 
 ]
 
-for page in pages:
-	print(page["title"])
+
+
+
+
+def main():
+	base = read_base_html()
+#	blog = open("./contents/blog.html").read()
+#	index = open("./contents/index.html").read()
+#	resume = open("./contents/online_resume.html").read()
+
+	#title = title()
+
+#	blog_html = base.replace("{{content}}", blog)
+#	index_html = base.replace("{{content}}", index)
+#	resume_html = base.replace("{{content}}", resume)
+#	open("docs/blog.html", "w+").write(blog_html)
+#	open("docs/index.html", "w+").write(index_html)
+#	open("docs/online_resume.html", "w+").write(resume_html)
+
+	for page in pages:
+		html = open(page['filename']).read()
+		page_html = base.replace("{{content}}", html)
+		open(page['output'], "w+").write(page_html)
+	
+	
+
+main()
 	
 
